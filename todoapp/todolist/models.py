4a0d80e3fe17fd61017d473c5cb131e9e15b7026
Evgeny <ytrokinterteiment@gmail.com>
2023-04-11 14:37:02 +0200

    Create model and migrations.

--- todoapp/todolist/models.py
+++ todoapp/todolist/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class ToDo(models.Model):
+    title = models.CharField("Task name", max_length=100)
+    description = models.CharField("Task description", max_length=200)
+    is_complete = models.BooleanField("Complete", default=False)
+
+    class Meta:
+        verbose_name = "Task"
+        verbose_name_plural = "Tasks"
+
+    def __str__(self):
+        return f"{self.title} - {self.description}"
