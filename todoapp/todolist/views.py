dbecd7902a3760dcd07ddb1ebd4b41616cc11513
Evgeny <ytrokinterteiment@gmail.com>
2023-04-16 17:22:14 +0200

    create notification and edit 'edit' view

--- todoapp/todolist/views.py
+++ todoapp/todolist/views.py
@@ -3,8 +3,25 @@ from django.http import HttpResponse
 from .models import ToDo
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
+from django.shortcuts import render
+from django.contrib import messages
+
+
+def edit(request, todo_id):
+    todo = ToDo.objects.get(id=todo_id)
+    if request.method == 'POST':
+        todo.title = request.POST['title']
+        todo.description = request.POST['description']
+        todo.deadline = request.POST['deadline']
+        todo.save()
+        messages.success(request, 'Task successfully edited!')
+        return redirect('index')
+    context = {"todo": todo}
+    return render(request, 'todoapp/edit.html', context)
+
+
 def index(request):
-    todos = ToDo.objects.all()
+    todos = ToDo.objects.all().order_by("deadline")
     return render(request, "todoapp/index.html", {"todolist": todos, "title": "Main page"})
 
 
@@ -13,18 +30,10 @@ def index(request):
 def add(request):
     title = request.POST["title"]
     description = request.POST["description"]
-    todo = ToDo(title=title, description=description)
+    deadline = request.POST["deadline"]
+    todo = ToDo(title=title, description=description, deadline=deadline)
     todo.save()
-    return redirect("index")
-
-
-@require_http_methods(["POST"])
-@csrf_exempt
-def edit(request, todo_id):
-    todo = ToDo.objects.get(id=todo_id)
-    todo.title = request.POST["title"]
-    todo.description = request.POST["description"]
-    todo.save()
+    messages.success(request, 'Task successfully added!')
     return redirect("index")
 
 
@@ -32,12 +41,14 @@ def update(request, todo_id):
     todo = ToDo.objects.get(id=todo_id)
     todo.is_complete = not todo.is_complete
     todo.save()
+    messages.success(request, 'Task successfully updated!')
     return redirect("index")
 
 
 def delete(request, todo_id):
     todo = ToDo.objects.get(id=todo_id)
     todo.delete()
+    messages.success(request, 'Task successfully deleted!')
     return redirect("index")
 
 
